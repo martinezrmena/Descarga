0400cea07fc3f4e9e251e9783e19107191f5f18b
Rafael Martinez Mena <rmartinez@cgclatam.com>
2018-10-06 19:20:03 -0600

    Cambios nuevos

--- app/src/main/java/usonsonate/com/descargaweb/MainActivity.java
+++ app/src/main/java/usonsonate/com/descargaweb/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
                 .centerInside()
                 .into(img);
 
+        //mas cambios para probar
     }
 
     //cambios para probar
